f578b134535ded6d631cf8f481a57cda6d41c484
Jan Heuer <janheuer2211@gmail.com>
2013-04-03 10:40:40 +0200

    make it localized

---
+++ application/locales/de.php
@@ -0,0 +1,10 @@
+<?php
+    return array(
+		'text_bidding_bidding_title' => 'Title',
+		'text_bidding_bidding_teaser' => 'kurze Beschreibung',
+		'text_bidding_bidding_text' => 'lange Beschreibung',
+		'label_bidding_bidding_title' => 'Title',
+		'label_bidding_bidding_teaser' => 'kurze Beschreibung',
+		'label_bidding_bidding_text' => 'lange Beschreibung',
+    );
+?>
