140f2c14b4242b5be32fd24e6a334081b22f2bf1
Jan Heuer <janheuer2211@gmail.com>
2013-04-03 13:06:28 +0200

    working navigation

--- application/locales/de.php
+++ application/locales/de.php
@@ -6,5 +6,6 @@
 		'label_bidding_bidding_title' => 'Title',
 		'label_bidding_bidding_teaser' => 'kurze Beschreibung',
 		'label_bidding_bidding_text' => 'lange Beschreibung',
+		'submit_bidding_bidding_create' => 'Speichern',
     );
 ?>
